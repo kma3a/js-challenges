34024761a9393ae1c792a99c7b1a17188a49f528
Kelsey Ando <kma3a@virginia.edu>
2018-07-26 23:29:19 -0400

    inital commit and adding some code for palindrom challenge

---
+++ palindrome/palindrome.js
@@ -0,0 +1,7 @@
+function palindrom(word) {
+}
+
+console.log(palindrom("tacocat") === true);
+console.log(palindrom("tacocat") === true);
+//console.log(palindrom("tacobat") === false);
+//console.log(palindrom("TacOCat") === true);
