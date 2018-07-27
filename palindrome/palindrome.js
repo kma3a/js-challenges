94e5528a0490e0d65a1644fa92adff35f03a42d9
Kelsey Ando <kma3a@virginia.edu>
2018-07-26 23:53:28 -0400

    adding the inital solution without punctuation ect

--- palindrome/palindrome.js
+++ palindrome/palindrome.js
@@ -1,7 +1,15 @@
 function palindrom(word) {
+  var length = word.length - 1;
+  var half = Math.ceil(length/2);
+  for(let i = 0; i < half; i ++) {
+    if(word[i] !== word[length - i]) {
+      return false;
+    }
+  }
+  return true;
 }
 
 console.log(palindrom("tacocat") === true);
-console.log(palindrom("tacocat") === true);
-//console.log(palindrom("tacobat") === false);
+console.log(palindrom("racecar") === true);
+console.log(palindrom("tacobat") === false);
 //console.log(palindrom("TacOCat") === true);
