dda236f9f02cff82441be2f3741ba6ca727bcb26
Kelsey Ando <kma3a@virginia.edu>
2018-08-04 21:37:25 -0400

    adding ways to get rid of punctuation

--- palindrome/palindrome.js
+++ palindrome/palindrome.js
@@ -1,8 +1,9 @@
 function palindrom(word) {
+  word = word.replace(/[\W]/g, "");
   var length = word.length - 1;
   var half = Math.ceil(length/2);
   for(let i = 0; i < half; i ++) {
-    if(word[i] !== word[length - i]) {
+    if(word[i].toLowerCase() !== word[length - i].toLowerCase()) {
       return false;
     }
   }
@@ -12,4 +13,7 @@ function palindrom(word) {
 console.log(palindrom("tacocat") === true);
 console.log(palindrom("racecar") === true);
 console.log(palindrom("tacobat") === false);
-//console.log(palindrom("TacOCat") === true);
+console.log(palindrom("TacOCat") === true);
+console.log(palindrom("put it up") === true);
+console.log(palindrom("TacOCat!") === true);
+console.log(palindrom("TacO Cat!") === true);
