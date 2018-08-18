a1372363d31f389f17f299635be2d783c130d7a3
Kelsey Ando <kma3a@virginia.edu>
2018-08-18 08:44:49 -0400

    changing how we are doing the showGame so that we can view it better

--- bowlingGame/bowling.js
+++ bowlingGame/bowling.js
@@ -4,32 +4,32 @@ class BowlingGame {
     this.game = [];
   }
   addPins(num) {
-    if(this.round === 9) {
+    if(this.round > 0) {
+      var previousRounds = this.round - 2 < 0 ? 0 : this.round - 2;
+      for(previousRounds; previousRounds < this.round; previousRounds ++ ) {
+        if(this.game[previousRounds].morePinsAmount() > 0) {
+          this.game[previousRounds].addTotal(num);
+        }
+      }
     }
     if(!this.game[this.round]){
       this.game[this.round] = new Frame(num, this.round);
-      if(this.round > 0 && (this.game[this.round-1].hasStrike() || this.game[this.round-1].hasSpare())) {
-       this.game[this.round-1].addTotal(num);
-      }
       if(num === 10) {
         this.round++;
       }
       return;
     }
     this.game[this.round].addPins(num);
-    if(this.round > 0 && this.game[this.round-1].hasStrike()) {
-     this.game[this.round-1].addTotal(num);
-    }
     this.round ++;
   }
   showGame() {
     var total = 0;
+    var game = "";
     this.game.forEach(function(frame){
-      console.log(frame.printFrame());
       total += frame.getTotal();
-      console.log(total);
+      game += frame.printFrame() + "\n" + total +"\n";
     });
-    console.log(total);
+    console.log( game + "\nTotal: " +  total);
   }
 }
 
@@ -39,36 +39,32 @@ class Frame {
     this.total = 0;
     this.pins = [];
     this.addPins(num);
-    this.strike = false;
-    this.spare = false;
-    if(num === 10) {
-      this.strike = true;
-      this.addPins(0);
+    this.needsMorePins = 0;
+    if(num === 10 && this.round !== 9) {
+      this.needsMorePins = 2;
+      this.pins = [10, 0];
     }
   }
   addPins(pins) {
     this.pins[this.pins.length] = pins;
     this.total += pins;
     if(this.pins.length === 2 && this.total === 10) {
-      this.spare = true;
+      this.needsMorePins = 1;
     }
 
   }
   printFrame() {
-    console.log("Frame ", this.round);
-    console.log(this.pins);
+    return "Frame " +  this.round + "\n" + "[" + this.pins[0] + "," + this.pins[1] + "]";
   }
   getTotal() {
     return this.total;
   }
   addTotal(num) {
     this.total += num;
+    this.needsMorePins--;
   }
-  hasStrike() {
-    return this.strike;
-  }
-  hasSpare() {
-    return this.spare;
+  morePinsAmount() {
+    return this.needsMorePins;
   }
 }
 
@@ -82,11 +78,9 @@ currentGame.addPins(10);
 currentGame.addPins(6);
 currentGame.addPins(4);
 //round 4
-currentGame.addPins(8);
-currentGame.addPins(1);
+currentGame.addPins(10);
 //round 5
-currentGame.addPins(4);
-currentGame.addPins(2);
+currentGame.addPins(10);
 //round 26
 currentGame.addPins(5);
 currentGame.addPins(4);
