316afc533c133e5d988af6de7c6235c3d7fcd9f8
Kelsey Ando <kma3a@virginia.edu>
2018-08-18 20:41:10 -0400

    getting the code to work with third frame and refactoring a bit

--- bowlingGame/bowling.js
+++ bowlingGame/bowling.js
@@ -2,25 +2,37 @@ class BowlingGame {
   constructor() {
     this.round = 0;
     this.game = [];
+    this.gameOver = false;
   }
   addPins(num) {
+    if(this.gameOver) {
+      console.log("Game Over!");
+      return;
+    }
     if(this.round > 0) {
       var previousRounds = this.round - 2 < 0 ? 0 : this.round - 2;
       for(previousRounds; previousRounds < this.round; previousRounds ++ ) {
-        if(this.game[previousRounds].morePinsAmount() > 0) {
+        if(!this.game[previousRounds].isFinished()) {
           this.game[previousRounds].addTotal(num);
         }
       }
     }
     if(!this.game[this.round]){
-      this.game[this.round] = new Frame(num, this.round);
-      if(num === 10) {
+      this.game[this.round] = new Frame(this.round);
+      this.game[this.round].addPins(num);
+      if(num === 10 && this.round < 9) {
         this.round++;
       }
       return;
     }
     this.game[this.round].addPins(num);
-    this.round ++;
+    this.checkGameOver();
+    if(this.round < 9) {
+      this.round ++;
+    }
+  }
+  checkGameOver() {
+    this.gameOver = this.round === 9 && this.game[this.round].isFinished();
   }
   showGame() {
     var total = 0;
@@ -34,34 +46,56 @@ class BowlingGame {
 }
 
 class Frame {
-  constructor(num, round) {
+  constructor(round, isLast) {
     this.round = round + 1;
     this.total = 0;
     this.pins = [];
-    this.addPins(num);
-    this.needsMorePins = 0;
-    if(num === 10 && this.round !== 9) {
-      this.needsMorePins = 2;
-      this.pins = [10, 0];
-    }
+    this.needsMorePins = isLast ? 3 : 2;
+    this.addPinsToTotal = 0;
+    this.finished = false;
   }
   addPins(pins) {
+    this.total += pins
+    if(this.pins.length === 0 && pins === 10) {
+      if(this.round < 10) {
+        this.addPinsToTotal = 2;
+        this.pins = [10, 0];
+      }
+      if(this.round === 10) {
+        this.pins.push(10);
+      }
+      return;
+    }
+    this.needsMorePins--;
     this.pins[this.pins.length] = pins;
-    this.total += pins;
     if(this.pins.length === 2 && this.total === 10) {
       this.needsMorePins = 1;
     }
-
+    this.checkFinished();
   }
   printFrame() {
-    return "Frame " +  this.round + "\n" + "[" + this.pins[0] + "," + this.pins[1] + "]";
+    return "Frame " +  this.round + "\n" + "[" + this.showPins() +"]";
   }
   getTotal() {
     return this.total;
   }
+  showPins() {
+    var showPin = "";
+    this.pins.forEach(function(currentPins, index, array) {
+      showPin += index === array.length - 1 ? currentPins : currentPins + ", "; 
+    });
+    return showPin;
+  }
+  checkFinished() {
+    this.finished = this.addPinsToTotal === 0 && this.needsMorePins === 0;
+  }
+  isFinished() {
+    return this.finished;
+  }
   addTotal(num) {
     this.total += num;
-    this.needsMorePins--;
+    this.addPinsToTotal--;
+    this.checkFinished();
   }
   morePinsAmount() {
     return this.needsMorePins;
@@ -94,8 +128,10 @@ currentGame.addPins(1);
 currentGame.addPins(4);
 currentGame.addPins(2);
 //round 10
-currentGame.addPins(4);
-currentGame.addPins(2);
 
+currentGame.addPins(10);
+currentGame.addPins(4);
+currentGame.addPins(4);
+currentGame.addPins(4);
 
 currentGame.showGame();
